d73780b269251f63ea0e0edca51f6771eb85396f
KODnC <kocd726@163.com>
2017-03-27 13:01:36 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/example/coolweather/gson/AQI.java
@@ -0,0 +1,18 @@
+package com.example.coolweather.gson;
+
+/**
+ * Created by pc on 2017/3/25.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+        public String aqi;
+
+        public String pm25;
+    }
+
+
+}
